e8ada78ad2e87206e0cc9cc7bf9f704cba5c0278
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2023-05-25 17:41:54 +0100

    Update index.js
    Added /removelast endpoint

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.post('/getlast', (req, res) => {
   res.json({ result });
 });
 
+// Removes the last X characters from a string, where X is the number passed to count
+app.post('/removelast', (req, res) => {
+  const { string, count } = req.body;
+  const result = string.substring(0, string.length - count);
+  res.json({ result });
+});
+
 //gets the specified characters in a string from a start point and end point using start, end
 app.post('/getsubstring', (req, res) => {
     const { string, start, end } = req.body;
