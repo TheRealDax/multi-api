232f323bf7b5544314d5849e588d0e96c7c7ab19
Dax <67021644+TheRealDax@users.noreply.github.com>
2023-06-08 14:36:38 +0100

    Update index.js
    added /convertnum endpoint

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ function getDateFormat(format) {
   }
 }
 
+// formats the number into a currency format, eg: 1000 = 1,000
 app.post('/currencyformat', (req, res) => {
   const { number } = req.body;
 
@@ -105,6 +106,63 @@ app.post('/currencyformat', (req, res) => {
   const currencyFormat = Number(number).toLocaleString();
 
   res.json({ result: currencyFormat });
+});
+
+//converts numbers from full to short or vice versa, eg: 1000 = 1k OR 1k = 1000
+app.post('/convertnum', (req, res) => {
+  const { number } = req.body;
+
+  // Conversion from number to shortened format
+  if (!isNaN(number)) {
+    const num = parseFloat(number);
+    let result = number;
+
+    if (num >= 1000000000000) {
+      if (num % 1 === 0) {
+        result = (num / 1000000000000) + 't';
+      } else {
+        result = (num / 1000000000000).toFixed(2) + 't';
+      }
+    } else if (num >= 1000000000) {
+      if (num % 1 === 0) {
+        result = (num / 1000000000) + 'b';
+      } else {
+        result = (num / 1000000000).toFixed(2) + 'b';
+      }
+    } else if (num >= 1000000) {
+      if (num % 1 === 0) {
+        result = (num / 1000000) + 'm';
+      } else {
+        result = (num / 1000000).toFixed(2) + 'm';
+      }
+    } else if (num >= 1000) {
+      if (num % 1 === 0) {
+        result = (num / 1000) + 'k';
+      } else {
+        result = (num / 1000).toFixed(2) + 'k';
+      }
+    }
+
+    res.json({ result });
+  }
+  
+  // Conversion from shortened format to number
+  else {
+    const numStr = number.toLowerCase();
+    let result = number;
+
+    if (numStr.endsWith('t')) {
+      result = parseFloat(numStr) * 1000000000000;
+    } else if (numStr.endsWith('b')) {
+      result = parseFloat(numStr) * 1000000000;
+    } else if (numStr.endsWith('m')) {
+      result = parseFloat(numStr) * 1000000;
+    } else if (numStr.endsWith('k')) {
+      result = parseFloat(numStr) * 1000;
+    }
+
+    res.json({ result });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
