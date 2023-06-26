36ab4d1f921324b9574387f7d548ea05597ace79
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2023-06-26 08:32:01 +0100

    Update test.js

--- endpoints/test.js
+++ endpoints/test.js
@@ -2,10 +2,13 @@
 const test = async (req, res) => {
     const { serverid, userid } = req.body;
     
+    //const payload = { serverid: `${serverid}`, userid: `${userid}` }
+
     console.log(serverid);
     console.log(userid);
+
   
-    res.json({ serverid, userid });
+    res.json({ serverid: `${serverid}`, userid: `${userid}` });
   };
 
   module.exports = test;
