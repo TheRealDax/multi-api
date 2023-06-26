12b76485cbe36a6a6bcfde1995a976c91483dc76
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2023-06-26 09:46:52 +0100

    Update test.js

--- endpoints/test.js
+++ endpoints/test.js
@@ -1,13 +1,15 @@
 // Non-functional test endpoint - don't bother.
 const test = async (req, res) => {
     const { serverid, userid } = req.body;
-    
-    console.log(serverid);
+
+    const serverId = `${serverid}`;
+
+    console.log(serverId);
     console.log(userid);
   
     // Perform operations on the serverIdString and userIdString values as needed
     
-    res.json({ serverid, userid });
+    res.json({ serverId, userid });
   };
 
   module.exports = test;
