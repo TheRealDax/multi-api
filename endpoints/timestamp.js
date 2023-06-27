f01bc38122a90682485ba170e5cd261bee563726
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2023-06-27 14:52:12 +0100

    Update timestamp.js

--- endpoints/timestamp.js
+++ endpoints/timestamp.js
@@ -2,22 +2,32 @@ const moment = require('moment');
 
 //generate a unix timestamp based on a specific date and time or days in the future
 const timestamp = async (req, res) => {
-    const { date, time, days, format } = req.body;
+    const { date, time, days, format, timestamp, offset} = req.body;
   
-    let timestamp;
+    let convertedTimestamp;
   
     if (date && time) {
       const datetime = `${date} ${time}`;
       const dateFormat = getDateFormat(format);
-      timestamp = moment(datetime, dateFormat).unix();
+      convertedTimestamp = moment(datetime, dateFormat).unix();
+
     } else if (days) {
       const futureDate = moment().add(days, 'days');
-      timestamp = futureDate.unix();
+      convertedTimestamp = futureDate.unix();
+
+    } else if (!date && !time && timestamp) {
+      if (offset) {
+        const offsetTimestamp = moment(timestamp).add(offset, 'hours');
+        convertedTimestamp = offsetTimestamp.format('YYYY-MM-DD HH:mm:ss');
+      } else {
+        convertedTimestamp = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+      }
+
     } else {
       return res.status(400).json({ error: 'Invalid request. Please provide either date and time or days parameter.' });
     }
   
-    res.json({ timestamp });
+    res.json({ convertedTimestamp });
   };
 
   //funtion to decide the date/time format. Default is EU if format parameter is not passed.
@@ -26,6 +36,9 @@ function getDateFormat(format) {
       case 'US':
         return ['MM/DD/YYYY h:mmA', 'MM/DD/YYYY HH:mm'];
       case 'EU':
+        return ['DD/MM/YYYY h:mmA', 'DD/MM/YYYY HH:mm'];
+      case 'ISO':
+        return ['YYYY-MM-DD HH:mm:ss'];
       default:
         return ['DD/MM/YYYY h:mmA', 'DD/MM/YYYY HH:mm'];
     }
