8ee4eec7790a349abf4182a43d196c394d951a16
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2023-07-10 21:16:06 +0100

    Update sendEmails.js

--- Genbot/sendEmails.js
+++ Genbot/sendEmails.js
@@ -4,15 +4,21 @@ const Base64 = require('js-base64').Base64;
 const fs = require('fs');
 
 const sendEmails = async (req, res) => {
-	const { threadid, message, email, to, subject } = req.body;
+	const { threadid, message } = req.body;
 
 	const oauth2Client = new google.auth.OAuth2(process.env.G_CLIENT_ID, process.env.G_CLIENT_SECRET, process.env.G_REDIRECT_URL);
+
+	const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+	const thread = await gmail.users.threads.get({ userId: 'me', id: threadid });
+	const email = thread.data.messages[0].payload.headers.find((header) => header.name === 'From').value;
+	const to = thread.data.messages[0].payload.headers.find((header) => header.name === 'To').value;
+	const subject = thread.data.messages[0].payload.headers.find((header) => header.name === 'Subject').value;
 
 	const db = await getDB('gmailDiscord');
 	const usersCollection = db.collection('users');
 	//const emailCollection = db.collection('emails');
 	const user = await usersCollection.findOne({ email: email });
-	if(!user) {
+	if (!user) {
 		console.error('User not found');
 		res.status(404).send('Error sending email.');
 		return;
@@ -39,9 +45,30 @@ const sendEmails = async (req, res) => {
 		await fs.promises.writeFile(`tokens_${user.email}.json`, JSON.stringify(refreshedTokens, null, 2));
 	}
 
-	const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
-	// create a base64url encoded string
-	const encodedMessage = Base64.encodeURI(`To: ${to}\nContent-Type: text/plain; charset=UTF-8\nSubject: Re: ${subject}\n\n${message}`);
+	const originalMessageResponse = await gmail.users.messages.get({
+		userId: 'me',
+		id: threadid,
+		format: 'full',
+	});
+
+	const originalMessageData = originalMessageResponse.data.payload.body.data;
+	const originalMessage = Buffer.from(originalMessageData, 'base64').toString('utf8');
+	const originalMessageHeaders = originalMessageResponse.data.payload.headers;
+
+	let originalFrom = '';
+	let originalDate = '';
+	for (let i = 0; i < originalMessageHeaders.length; i++) {
+		if (originalMessageHeaders[i].name === 'From') {
+			originalFrom = originalMessageHeaders[i].value;
+		}
+		if (originalMessageHeaders[i].name === 'Date') {
+			originalDate = originalMessageHeaders[i].value;
+		}
+	}
+
+	const quotedOriginalMessage = `On ${originalDate}, ${originalFrom} wrote:\n> ${originalMessage.replace(/\n/g, '\n> ')}`;
+	const replyMessage = `${message}\n\n${quotedOriginalMessage}`;
+	const encodedMessage = Base64.encodeURI(`To: ${to}\nContent-Type: text/plain; charset=UTF-8\nSubject: Re: ${subject}\n\n${replyMessage}`);
 
 	try {
 		await gmail.users.messages.send({
