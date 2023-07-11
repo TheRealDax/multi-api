8866f5b0d0deed699ce20279448fd210e479a725
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2023-07-11 12:57:30 +0100

    Update sendEmails.js

--- Genbot/sendEmails.js
+++ Genbot/sendEmails.js
@@ -68,7 +68,8 @@ const sendEmails = async (req, res) => {
 		const email = thread.data.messages[0].payload.headers.find((header) => header.name === 'From').value;
 		//const to = thread.data.messages[0].payload.headers.find((header) => header.name === 'To').value;
 		const subject = thread.data.messages[0].payload.headers.find((header) => header.name === 'Subject').value;
-
+		console.log('originalMessageResponse:', JSON.stringify(originalMessageResponse, null, 2));
+		
 		const originalMessageData = originalMessageResponse.data.payload.body.data;
 		const originalMessage = Buffer.from(originalMessageData, 'base64').toString('utf8');
 		const originalMessageHeaders = originalMessageResponse.data.payload.headers;
