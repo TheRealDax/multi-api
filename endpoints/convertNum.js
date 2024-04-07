d634868af33a3dd70815dc1f09dfa2a96acf643e
TheRealDax <67021644+TheRealDax@users.noreply.github.com>
2024-04-07 12:29:20 +0100

    changes

--- endpoints/convertNum.js
+++ endpoints/convertNum.js
@@ -32,73 +32,72 @@
 
 //converts numbers from full to short or vice versa, eg: 1000 = 1k OR 1k = 1000
 const convertNum = async (req, res) => {
-  try{
+	try {
+		if (!req.query || req.query.number === undefined) {
+			return res.status(400).json({ error: 'number not specified' });
+		}
 
-  if (!req.query || req.query.number === undefined) {
-    return res.status(400).json({ error: 'number not specified' });
-}
+		const { number } = req.query;
 
-    const { number } = req.query;
+		if (isNaN(number) && !number.toLowerCase().match(/[kmbt]$/)) {
+			return res.status(400).json({ error: 'Invalid number or unsupported string format' });
+		}
 
-     if (isNaN(number) && !number.toLowerCase().match(/[kmbt]$/)) {
-      return res.status(400).json({ error: 'Invalid number or unsupported string format' });
-  }
-  
-    // Conversion from number to shortened format
-    if (!isNaN(number)) {
-      const num = parseFloat(number);
-      let result = number;
-  
-      if (num >= 1000000000000) {
-        if (num % 10 === 0) {
-          result = (Math.floor(num / 100000000) / 10000) + 't';
-        } else {
-          result = (num / 1000000000000).toFixed(2) + 't';
-        }
-      } else if (num >= 1000000000) {
-        if (num % 10 === 0) {
-          result = (Math.floor(num / 1000000) / 1000) + 'b';
-        } else {
-          result = (num / 1000000000).toFixed(2) + 'b';
-        }
-      } else if (num >= 1000000) {
-        if (num % 10 === 0) {
-          result = (Math.floor(num / 10000) / 100) + 'm';
-        } else {
-          result = (num / 1000000).toFixed(2) + 'm';
-        }
-      } else if (num >= 1000) {
-        if (num % 1 === 0) {
-          result = (num / 1000) + 'k';
-        } else {
-          result = (num / 1000).toFixed(2) + 'k';
-        }
-      }
-  
-      return res.json({ result });
-    }
-    
-    // Conversion from shortened format to number
-    else {
-      const numStr = number.toLowerCase();
-      let result = number;
-  
-      if (numStr.endsWith('t')) {
-        result = parseFloat(numStr) * 1000000000000;
-      } else if (numStr.endsWith('b')) {
-        result = parseFloat(numStr) * 1000000000;
-      } else if (numStr.endsWith('m')) {
-        result = parseFloat(numStr) * 1000000;
-      } else if (numStr.endsWith('k')) {
-        result = parseFloat(numStr) * 1000;
-      }
-  
-      return res.json({ result });
-    }
+		// Conversion from number to shortened format
+		if (!isNaN(number)) {
+			const num = parseFloat(number);
+			let result = number;
 
-  } catch (err){
-    console.error('Error:', err);
-    return res.status(500).json({ error: `${err}` });
-  }};
+			if (num >= 1000000000000) {
+				if (num % 10 === 0) {
+					result = Math.floor(num / 100000000) / 10000 + 't';
+				} else {
+					result = (num / 1000000000000).toFixed(2) + 't';
+				}
+			} else if (num >= 1000000000) {
+				if (num % 10 === 0) {
+					result = Math.floor(num / 1000000) / 1000 + 'b';
+				} else {
+					result = (num / 1000000000).toFixed(2) + 'b';
+				}
+			} else if (num >= 1000000) {
+				if (num % 10 === 0) {
+					result = Math.floor(num / 10000) / 100 + 'm';
+				} else {
+					result = (num / 1000000).toFixed(2) + 'm';
+				}
+			} else if (num >= 1000) {
+				if (num % 1 === 0) {
+					result = num / 1000 + 'k';
+				} else {
+					result = (num / 1000).toFixed(2) + 'k';
+				}
+			}
 
-  module.exports = convertNum;
+			return res.status(200).json({ result });
+		}
+
+		// Conversion from shortened format to number
+		else {
+			const numStr = number.toLowerCase();
+			let result = number;
+
+			if (numStr.endsWith('t')) {
+				result = parseFloat(numStr) * 1000000000000;
+			} else if (numStr.endsWith('b')) {
+				result = parseFloat(numStr) * 1000000000;
+			} else if (numStr.endsWith('m')) {
+				result = parseFloat(numStr) * 1000000;
+			} else if (numStr.endsWith('k')) {
+				result = parseFloat(numStr) * 1000;
+			}
+
+			return res.status(200).json({ result });
+		}
+	} catch (err) {
+		console.error('Error:', err);
+		return res.status(500).json({ error: `${err}` });
+	}
+};
+
+module.exports = convertNum;
--- index.js
+++ index.js
@@ -4,54 +4,46 @@ dotenv.config();
 const express = require('express');
 const swaggerui = require('swagger-ui-express');
 const specs = require('./config/swagger');
-const db = require('./functions/db');
 
 const PORT = process.env.PORT || 3000;
-async function Init() {
-	await db.connect();
+const app = express();
 
-	const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+// Endpoints
+const getFirst = require('./endpoints/getFirst');
+const getLast = require('./endpoints/getLast');
+const removeLast = require('./endpoints/removeLast');
+const getSubString = require('./endpoints/getSubString');
+const timestamp = require('./endpoints/timestamp');
+const currencyFormat = require('./endpoints/currencyFormat');
+const convertNum = require('./endpoints/convertNum');
+const transcript = require('./endpoints/transcript');
+const getRoleCount = require('./endpoints/getRoleCount');
+const memberRoles = require('./endpoints/memberRoles');
+const highestRole = require('./endpoints/highestRole');
+const base64 = require('./endpoints/base64');
 
-	// Endpoints
-	const getFirst = require('./endpoints/getFirst');
-	const getLast = require('./endpoints/getLast');
-	const removeLast = require('./endpoints/removeLast');
-	const getSubString = require('./endpoints/getSubString');
-	const timestamp = require('./endpoints/timestamp');
-	const currencyFormat = require('./endpoints/currencyFormat');
-	const convertNum = require('./endpoints/convertNum');
-	const transcript = require('./endpoints/transcript');
-	const getRoleCount = require('./endpoints/getRoleCount');
-	const memberRoles = require('./endpoints/memberRoles');
-	const highestRole = require('./endpoints/highestRole');
-	const vcRecord = require('./endpoints/vcrecord');
-	const base64 = require('./endpoints/base64');
+// POST requests
+app.post('/getfirst', getFirst);
+app.post('/getlast', getLast);
+app.post('/removelast', removeLast);
+app.post('/getsubstring', getSubString);
+app.post('/timestamp', timestamp);
+app.post('/transcript', transcript);
+app.post('/base64', base64);
 
-	// POST requests
-	app.post('/getfirst', getFirst);
-	app.post('/getlast', getLast);
-	app.post('/removelast', removeLast);
-	app.post('/getsubstring', getSubString);
-	app.post('/timestamp', timestamp);
-	app.post('/transcript', transcript);
-	app.post('/base64', base64);
+// GET requests
+app.get('/getrolecount', getRoleCount);
+app.get('/convertnum', convertNum);
+app.get('/currencyformat', currencyFormat);
+app.get('/memberroles', memberRoles);
+app.get('/highestrole', highestRole);
 
-	// GET requests
-	app.get('/getrolecount', getRoleCount);
-	app.get('/convertnum', convertNum);
-	app.get('/currencyformat', currencyFormat);
-	app.get('/memberroles', memberRoles);
-	app.get('/highestrole', highestRole);
-	app.get('/vcrecord', vcRecord);
+// Use requests
+app.use('/docs', swaggerui.serve, swaggerui.setup(specs));
 
-	// Use requests
-	app.use('/docs', swaggerui.serve, swaggerui.setup(specs));
-
-	app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}`);
-	});
-}
-Init().catch(console.error);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
