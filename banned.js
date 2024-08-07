d07565bab2a59e1a3295acf915e6d537876b3b84
Dax <67021644+TheRealDax@users.noreply.github.com>
2024-08-07 09:29:56 +0100

    Update banned.js

--- banned.js
+++ banned.js
@@ -1,4 +1,4 @@
-const bannedServers = ['1193021133981765632'];
+const bannedServers = ['1193021133981765632', '275500976662773761'];
 
 const isBanned = async function (serverid) {
 	if (bannedServers.includes(serverid)) return true;
